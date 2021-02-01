280909f5f3fde29973afc0700f47062f2d0f36af
djc <1820862899@qq.com>
2021-02-02 00:38:13 +0800

    vue学习

--- spring-01-anno/src/test/java/com/djc/test/Test1.java
+++ spring-01-anno/src/test/java/com/djc/test/Test1.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Test1 {
+
     @Test
     public void test01(){
-
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config1.class);
         UserService userService = context.getBean("userService", UserService.class);
         System.out.println(userService.getUser());
-
     }
+
 }
